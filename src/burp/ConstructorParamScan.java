3797694fa8f786cce2e42e38876817259ce338dd
Gareth Heyes <gareth.heyes@portswigger.net>
2022-06-09 12:25:35 +0100

    Added markers to show attack request and native code response

--- src/burp/ConstructorParamScan.java
+++ src/burp/ConstructorParamScan.java
@@ -1,10 +1,15 @@
 package burp;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ConstructorParamScan extends ParamScan {
+
+    static final String nativeCodeRegex = "native.{1,10}code";
+    static public final String validProperty = "constructor";
+    static public final String invalidProperty = "constxructor";
 
     private final Integer MAX_RETRIES = 3;
 
@@ -16,24 +21,53 @@ public class ConstructorParamScan extends ParamScan {
     List<IScanIssue> doScan(IHttpRequestResponse baseRequestResponse, IScannerInsertionPoint insertionPoint) {
         IHttpService service = baseRequestResponse.getHttpService();
 
-        byte[] attackReq = insertionPoint.buildRequest(("constructor").getBytes());
+        byte[] attackReq = insertionPoint.buildRequest((validProperty).getBytes());
         Resp attackResp = request(service, attackReq, MAX_RETRIES);
-
         if(hasNativeCode(attackResp)) {
-            byte[] nullifyReq = insertionPoint.buildRequest(("constxructor").getBytes());
+            byte[] nullifyReq = insertionPoint.buildRequest((invalidProperty).getBytes());
             Resp nullifyResp = request(service, nullifyReq, MAX_RETRIES);
             if(!hasNativeCode(nullifyResp)) {
-                ServerSidePrototypePollutionScan.reportIssue("ConstructorParamScan leaking native code", "The parameter "+insertionPoint.getInsertionPointName()+" was identified that seems to indicate that it is being reference in a JavaScript property using the constructor property name.", "Low", "Firm", ".", baseRequestResponse.getRequest(), attackResp, nullifyResp);
+                IHttpRequestResponseWithMarkers attackRespWithMarkers = Utilities.callbacks.applyMarkers(attackResp.getReq(), getMatches(attackResp.getReq().getRequest(), validProperty.getBytes()),getNativeCodeMarkerPositions(attackResp));
+                IHttpRequestResponseWithMarkers nullifyRespWithMarkers = Utilities.callbacks.applyMarkers(nullifyResp.getReq(), getMatches(nullifyResp.getReq().getRequest(), invalidProperty.getBytes()),null);
+                ServerSidePrototypePollutionScan.reportIssue("ConstructorParamScan leaking native code", "The parameter "+insertionPoint.getInsertionPointName()+" was identified that seems to indicate that it is being reference in a JavaScript property using the constructor property name.", "Low", "Firm", ".", baseRequestResponse.getRequest(), new Resp(attackRespWithMarkers), new Resp(nullifyRespWithMarkers));
             }
         }
 
         return null;
     }
 
+    static List<int[]> getNativeCodeMarkerPositions(Resp response) {
+        String responseStr = Utilities.helpers.bytesToString(response.getReq().getResponse());
+        Pattern p = Pattern.compile(nativeCodeRegex);
+        Matcher m = p.matcher(responseStr);
+        List<int[]> matches = new ArrayList<int[]>();
+        while (m.find()) {
+            matches.add(new int[] { m.start(), m.end() });
+        }
+        return matches;
+    }
+
     static Boolean hasNativeCode(Resp response) {
         String responseStr = Utilities.helpers.bytesToString(response.getReq().getResponse());
-        Pattern p = Pattern.compile("native.{1,10}code");
+        Pattern p = Pattern.compile(nativeCodeRegex);
         Matcher m = p.matcher(responseStr);
         return m.find();
+    }
+
+    static List<int[]> getMatches(byte[] response, byte[] match)
+    {
+        List<int[]> matches = new ArrayList<int[]>();
+
+        int start = 0;
+        while (start < response.length)
+        {
+            start = Utilities.helpers.indexOf(response, match, true, start, response.length);
+            if (start == -1)
+                break;
+            matches.add(new int[] { start, start + match.length });
+            start += match.length;
+        }
+
+        return matches;
     }
 }
--- src/burp/ConstructorScanAddParam.java
+++ src/burp/ConstructorScanAddParam.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class ConstructorScanAddParam extends Scan {
 
     private final Integer MAX_RETRIES = 3;
+
     ConstructorScanAddParam(String name) {
         super(name);
     }
@@ -20,15 +21,17 @@ public class ConstructorScanAddParam extends Scan {
     }
 
     public void doAttack(byte[] baseReq, IHttpService service, byte insertionPointType) {
-        IParameter parameter = Utilities.helpers.buildParameter(Utilities.helpers.urlEncode("constructor"), Utilities.helpers.urlEncode("constructor"), insertionPointType);
+        IParameter parameter = Utilities.helpers.buildParameter(Utilities.helpers.urlEncode(ConstructorParamScan.validProperty), Utilities.helpers.urlEncode(ConstructorParamScan.validProperty), insertionPointType);
         byte[] attackReq = Utilities.helpers.addParameter(baseReq, parameter);
         Resp attackResp = request(service, attackReq, MAX_RETRIES);
         if(ConstructorParamScan.hasNativeCode(attackResp)) {
-            IParameter nullifyParameter = Utilities.helpers.buildParameter(Utilities.helpers.urlEncode("constxructor"), Utilities.helpers.urlEncode("constxructor"), insertionPointType);
+            IParameter nullifyParameter = Utilities.helpers.buildParameter(Utilities.helpers.urlEncode(ConstructorParamScan.invalidProperty), Utilities.helpers.urlEncode(ConstructorParamScan.invalidProperty), insertionPointType);
             byte[] nullifyReq = Utilities.helpers.addParameter(baseReq, nullifyParameter);
             Resp nullifyResp = request(service, nullifyReq, MAX_RETRIES);
             if(!ConstructorParamScan.hasNativeCode(nullifyResp)) {
-                ServerSidePrototypePollutionScan.reportIssue("ConstructorScanAddParam leaking native code", "An added parameter constructor was added to the request and it indicates that this was being used to reference a JavaScript property.", "Low", "Firm", ".", baseReq, attackResp, nullifyResp);
+                IHttpRequestResponseWithMarkers attackRespWithMarkers = Utilities.callbacks.applyMarkers(attackResp.getReq(), ConstructorParamScan.getMatches(attackResp.getReq().getRequest(), ConstructorParamScan.validProperty.getBytes()),ConstructorParamScan.getNativeCodeMarkerPositions(attackResp));
+                IHttpRequestResponseWithMarkers nullifyRespWithMarkers = Utilities.callbacks.applyMarkers(nullifyResp.getReq(), ConstructorParamScan.getMatches(nullifyResp.getReq().getRequest(), ConstructorParamScan.invalidProperty.getBytes()),null);
+                ServerSidePrototypePollutionScan.reportIssue("ConstructorScanAddParam leaking native code", "An added parameter constructor was added to the request and it indicates that this was being used to reference a JavaScript property.", "Low", "Firm", ".", baseReq, new Resp(attackRespWithMarkers), new Resp(nullifyRespWithMarkers));
             }
         }
     }
