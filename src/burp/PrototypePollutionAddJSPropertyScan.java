27d5c517ca1acd4441dca3835a4337c05e3331d8
Gareth Heyes <gareth.heyes@portswigger.net>
2022-09-01 12:54:00 +0100

    Fixed nullptr

--- src/burp/PrototypePollutionAddJSPropertyScan.java
+++ src/burp/PrototypePollutionAddJSPropertyScan.java
@@ -74,9 +74,9 @@ public class PrototypePollutionAddJSPropertyScan extends Scan {
         String regex = Utilities.globalSettings.getString("vulnerable response regex");
 
         Resp attackResp = request(service, attackReq, PrototypePollutionBodyScan.MAX_RETRIES);
-        if(attackResp != null && PrototypePollutionJSPropertyParamScan.regexResponse(attackResp)) {
+        if(attackResp.getReq().getResponse() != null && PrototypePollutionJSPropertyParamScan.regexResponse(attackResp)) {
             Resp nullifyResp = request(service, nullifyReq, PrototypePollutionBodyScan.MAX_RETRIES);
-            if(nullifyResp != null && !PrototypePollutionJSPropertyParamScan.regexResponse(nullifyResp)) {
+            if(nullifyResp.getReq().getResponse() != null && !PrototypePollutionJSPropertyParamScan.regexResponse(nullifyResp)) {
                 IHttpRequestResponseWithMarkers attackRespWithMarkers = Utilities.callbacks.applyMarkers(attackResp.getReq(), PrototypePollutionJSPropertyParamScan.getMatches(attackResp.getReq().getRequest(), validProperty.getBytes()), PrototypePollutionJSPropertyParamScan.getRegexMarkerPositions(attackResp, regex));
                 IHttpRequestResponseWithMarkers nullifyRespWithMarkers = Utilities.callbacks.applyMarkers(nullifyResp.getReq(), PrototypePollutionJSPropertyParamScan.getMatches(nullifyResp.getReq().getRequest(), invalidProperty.getBytes()),null);
                 PrototypePollutionBodyScan.reportIssue("Add property scan using "+validProperty, "An added parameter "+validProperty+" was added to the request and a regex \""+regex+"\" was used to see if it causes a response difference.", "Low", "Firm", ".", baseReq, new Resp(attackRespWithMarkers), new Resp(nullifyRespWithMarkers));
