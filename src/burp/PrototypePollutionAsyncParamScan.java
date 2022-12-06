5ab0fef26070262e861620dd4131fd30239026d0
Gareth Heyes <gareth.heyes@portswigger.net>
2022-12-06 09:07:24 +0000

    Added Collaborator pingback functionality.

--- src/burp/PrototypePollutionAsyncParamScan.java
+++ src/burp/PrototypePollutionAsyncParamScan.java
@@ -2,6 +2,7 @@ package burp;
 
 import com.google.gson.JsonElement;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -22,6 +23,8 @@ public class PrototypePollutionAsyncParamScan extends ParamScan {
         for (Map.Entry<String, String[]> technique : techniques.entrySet()) {
             String attackType = technique.getKey();
             String attackInjection = technique.getValue()[1];
+            ArrayList<String> collabPayloads = new ArrayList<>();
+            technique.getValue()[1] = PrototypePollutionAsyncBodyScan.replacePlaceholderWithCollaboratorPayload(attackInjection, collabPayloads);
             String baseValue = insertionPoint.getBaseValue();
             byte[] attackReq;
 
@@ -39,7 +42,16 @@ public class PrototypePollutionAsyncParamScan extends ParamScan {
                 continue;
             }
             Utilities.out("Doing async param scan " + attackType + " attack");
-            request(service, attackReq, PrototypePollutionBodyScan.MAX_RETRIES);
+            Resp reqResp = request(service, attackReq, PrototypePollutionBodyScan.MAX_RETRIES);
+            int reqId = -1;
+            if(collabPayloads.size() > 0) {
+                reqId = BurpExtender.collab.addRequest(new MetaRequest(reqResp.getReq()));
+            }
+            for (String collabPayload : collabPayloads) {
+                if(reqId > 0) {
+                    BurpExtender.collab.addCollboratorPayload(collabPayload, reqId);
+                }
+            }
         }
     }
 }
